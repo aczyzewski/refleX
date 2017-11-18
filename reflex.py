3adef9e0afcc6485857a62cefb2882c720ffb47d
Dabrze@Minorlab <dariusz.brzezinski@cs.put.poznan.pl>
2017-11-18 15:58:16 -0500

    Learning Keras and playing with image augmentation

--- reflex.py
+++ reflex.py
@@ -14,6 +14,7 @@ from keras.layers import Dense, Dropout, Activation, Flatten
 from keras.layers import Conv2D, MaxPooling2D
 from keras.optimizers import Adam
 from keras.callbacks import Callback
+from keras.preprocessing import image
 
 SEED = 23
 MODEL_PATH = "reflex.h5"
@@ -21,15 +22,92 @@ np.random.seed(SEED)
 logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s: %(message)s', datefmt='%m/%d/%Y %H:%M:%S')
 
 
-class AccuracyHistory(Callback):
-    def on_train_begin(self, logs={}):
-        self.acc = []
+class ReflexDataGenerator(image.ImageDataGenerator):
+    '''
+    Provides uniform zoom in both directions
+    '''
+    def random_transform(self, x, seed=None):
+        img_row_axis = self.row_axis - 1
+        img_col_axis = self.col_axis - 1
+        img_channel_axis = self.channel_axis - 1
 
-    def on_epoch_end(self, batch, logs={}):
-        self.acc.append(logs.get('acc'))
+        if seed is not None:
+            np.random.seed(seed)
+
+        # use composition of homographies
+        # to generate final transform that needs to be applied
+        if self.rotation_range:
+            theta = np.pi / 180 * np.random.uniform(-self.rotation_range, self.rotation_range)
+        else:
+            theta = 0
+
+        if self.height_shift_range:
+            tx = np.random.uniform(-self.height_shift_range, self.height_shift_range) * x.shape[img_row_axis]
+        else:
+            tx = 0
+
+        if self.width_shift_range:
+            ty = np.random.uniform(-self.width_shift_range, self.width_shift_range) * x.shape[img_col_axis]
+        else:
+            ty = 0
+
+        if self.shear_range:
+            shear = np.random.uniform(-self.shear_range, self.shear_range)
+        else:
+            shear = 0
+
+        if self.zoom_range[0] == 1 and self.zoom_range[1] == 1:
+            zx, zy = 1, 1
+        else:
+            zx, zy = np.random.uniform(self.zoom_range[0], self.zoom_range[1], 2)
+
+        transform_matrix = None
+        if theta != 0:
+            rotation_matrix = np.array([[np.cos(theta), -np.sin(theta), 0],
+                                        [np.sin(theta), np.cos(theta), 0],
+                                        [0, 0, 1]])
+            transform_matrix = rotation_matrix
+
+        if tx != 0 or ty != 0:
+            shift_matrix = np.array([[1, 0, tx],
+                                     [0, 1, ty],
+                                     [0, 0, 1]])
+            transform_matrix = shift_matrix if transform_matrix is None else np.dot(transform_matrix, shift_matrix)
+
+        if shear != 0:
+            shear_matrix = np.array([[1, -np.sin(shear), 0],
+                                    [0, np.cos(shear), 0],
+                                    [0, 0, 1]])
+            transform_matrix = shear_matrix if transform_matrix is None else np.dot(transform_matrix, shear_matrix)
+
+        if zx != 1:
+            zoom_matrix = np.array([[zx, 0, 0],
+                                    [0, zx, 0],
+                                    [0, 0, 1]])
+            transform_matrix = zoom_matrix if transform_matrix is None else np.dot(transform_matrix, zoom_matrix)
+
+        if transform_matrix is not None:
+            h, w = x.shape[img_row_axis], x.shape[img_col_axis]
+            transform_matrix = image.transform_matrix_offset_center(transform_matrix, h, w)
+            x = image.apply_transform(x, transform_matrix, img_channel_axis,
+                                fill_mode=self.fill_mode, cval=self.cval)
+
+        if self.channel_shift_range != 0:
+            x = image.random_channel_shift(x,
+                                     self.channel_shift_range,
+                                     img_channel_axis)
+        if self.horizontal_flip:
+            if np.random.random() < 0.5:
+                x = image.flip_axis(x, img_col_axis)
+
+        if self.vertical_flip:
+            if np.random.random() < 0.5:
+                x = image.flip_axis(x, img_row_axis)
+
+        return x
 
 
-class Reflex():
+class Reflex:
     def __init__(self, img_x, img_y, num_classes, image_files, label_file):
         self.img_x = img_x
         self.img_y = img_y
@@ -82,31 +160,70 @@ class Reflex():
 
         return X_train, X_test, y_train, y_test
 
-    def train_model(self, X_train, y_train, verbose=1, save_model=True, model_name=MODEL_PATH,
-                    plot_learning_curve=True):
+    def train_model(self, X_train, y_train, X_val, y_val, verbose=1, save_model=True, model_name=MODEL_PATH,
+                    plot_learning_curve=True, epochs=10, batch_size=64):
         model = Sequential()
-        model.add(Conv2D(32, kernel_size=(5, 5), strides=(1, 1), activation='relu', input_shape=self.input_shape))
-        model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
-        model.add(Conv2D(64, (5, 5), activation='relu'))
-        model.add(MaxPooling2D(pool_size=(2, 2)))
+
+        model.add(Conv2D(64, kernel_size=3, strides=1, activation='relu', padding='same', input_shape=self.input_shape))
+        model.add(Conv2D(64, kernel_size=3, strides=1, activation='relu', padding='same', input_shape=self.input_shape))
+        model.add(MaxPooling2D(pool_size=2, strides=2))
+
+        model.add(Conv2D(128, kernel_size=3, strides=1, activation='relu', padding='same', input_shape=self.input_shape))
+        model.add(Conv2D(128, kernel_size=3, strides=1, activation='relu', padding='same', input_shape=self.input_shape))
+        model.add(MaxPooling2D(pool_size=2, strides=2))
+
+        model.add(Conv2D(256, kernel_size=3, strides=1, activation='relu', padding='same', input_shape=self.input_shape))
+        model.add(Conv2D(256, kernel_size=3, strides=1, activation='relu', padding='same', input_shape=self.input_shape))
+        model.add(Conv2D(256, kernel_size=3, strides=1, activation='relu', padding='same', input_shape=self.input_shape))
+        model.add(MaxPooling2D(pool_size=2, strides=2))
+
+        model.add(Conv2D(512, kernel_size=3, strides=1, activation='relu', padding='same', input_shape=self.input_shape))
+        model.add(Conv2D(512, kernel_size=3, strides=1, activation='relu', padding='same', input_shape=self.input_shape))
+        model.add(Conv2D(512, kernel_size=3, strides=1, activation='relu', padding='same', input_shape=self.input_shape))
+        model.add(MaxPooling2D(pool_size=2, strides=2))
+
+        model.add(Conv2D(512, kernel_size=3, strides=1, activation='relu', padding='same', input_shape=self.input_shape))
+        model.add(Conv2D(512, kernel_size=3, strides=1, activation='relu', padding='same', input_shape=self.input_shape))
+        model.add(Conv2D(512, kernel_size=3, strides=1, activation='relu', padding='same', input_shape=self.input_shape))
+        model.add(MaxPooling2D(pool_size=2, strides=2))
+
         model.add(Flatten())
-        model.add(Dense(1000, activation='relu'))
+        model.add(Dense(1024, activation='relu'))
         model.add(Dense(self.num_classes, activation='sigmoid'))
-        model.compile(loss='binary_crossentropy', optimizer=Adam(), metrics=['accuracy'])
 
-        history = AccuracyHistory()
+        model.compile(loss='binary_crossentropy', optimizer=Adam(decay=0.1), metrics=['accuracy'])
 
-        model.fit(X_train, y_train,
-                  batch_size=128,
-                  epochs=10,
-                  verbose=verbose,
-                  validation_split=0.1,
-                  callbacks=[history])
+        train_datagen = ReflexDataGenerator(
+            zoom_range=(1, 1.2),
+            horizontal_flip=True,
+            vertical_flip=True,
+            rotation_range=45,
+        )
+        train_iterator = train_datagen.flow(X_train, y_train, batch_size=batch_size, seed=SEED)
+        val_iterator = ReflexDataGenerator().flow(X_val, y_val, batch_size=batch_size, seed=SEED)
+
+        history = model.fit_generator(train_iterator,
+                                      epochs=epochs,
+                                      steps_per_epoch=max(len(X_train) / batch_size, 1),
+                                      verbose=verbose,
+                                      validation_data=val_iterator,
+                                      validation_steps=max(len(X_val) / batch_size, 1))
 
         if plot_learning_curve:
-            plt.plot(range(1, 11), history.acc)
-            plt.xlabel('Epochs')
-            plt.ylabel('Accuracy')
+            plt.plot(history.history['acc'])
+            plt.plot(history.history['val_acc'])
+            plt.title('model accuracy')
+            plt.ylabel('accuracy')
+            plt.xlabel('epoch')
+            plt.legend(['train', 'validation'], loc='upper left')
+            plt.show()
+
+            plt.plot(history.history['loss'])
+            plt.plot(history.history['val_loss'])
+            plt.title('model loss')
+            plt.ylabel('loss')
+            plt.xlabel('epoch')
+            plt.legend(['train', 'validation'], loc='upper left')
             plt.show()
 
         if save_model:
@@ -144,17 +261,19 @@ class Reflex():
                 best_threshold[i] = threshold[index[0][0]]
                 acc = []
         else:
-            best_threshold = np.ones(y_proba.shape[1])*0.5
+            best_threshold = np.ones(y_proba.shape[1]) * 0.5
 
         logging.info("Class thresholds: %s", best_threshold)
         y_pred = np.array([[1 if y_proba[i, j] >= best_threshold[j] else 0 for j in range(y_test.shape[1])] for i in
                            range(len(y_test))])
 
         logging.info("Hamming score: %.3f", self._hamming_score(y_test, y_pred))
-        logging.info("Exact match ratio: %.3f", metrics.accuracy_score(y_test, y_pred, normalize=True, sample_weight=None))
+        logging.info("Exact match ratio: %.3f",
+                     metrics.accuracy_score(y_test, y_pred, normalize=True, sample_weight=None))
         logging.info("Hamming loss: %.3f", metrics.hamming_loss(y_test, y_pred))
         logging.info("Micro-averaged precision: %.3f", metrics.precision_score(y_test, y_pred, average="micro"))
         logging.info("Micro-averaged recall: %.3f", metrics.recall_score(y_test, y_pred, average="micro"))
+        logging.info("Micro-averaged F-score: %.3f", metrics.f1_score(y_test, y_pred, average="micro"))
 
     def _hamming_score(self, y_true, y_pred):
         acc_list = []
@@ -176,5 +295,5 @@ if __name__ == "__main__":
     reflex = Reflex(img_x=300, img_y=300, num_classes=7, image_files=files, label_file="reflex.csv")
     reflex.load_files()
     X_train, X_test, y_train, y_test = reflex.split_data(test_ratio=0.1)
-    # reflex.train_model(X_train, y_train)
-    reflex.test_model(X_test, y_test)
+    reflex.train_model(X_train, y_train, X_val=X_test, y_val=y_test, model_name="reflex_aug.h5")
+    reflex.test_model(X_test, y_test, model_name="reflex_aug.h5")
